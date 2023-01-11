20cd3459ce2712a73c910287bea14aec80b39733
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-01-11 11:44:26 +0100

    Ch modules

--- internal/redirection/main.tf
+++ internal/redirection/main.tf
@@ -1,3 +1,7 @@
+locals {
+  s3_origin_id = "S3Origin"
+}
+
 resource "aws_s3_bucket" "static_website" {
   bucket = var.url_name
 
@@ -22,67 +26,63 @@ resource "aws_s3_bucket_website_configuration" "bucket_website" {
   }
 }
 
-# locals {
-#   s3_origin_id = "S3Origin"
-# }
+module "acm" {
+  source = "terraform-aws-modules/acm/aws"
 
-# module "acm" {
-#   source = "terraform-aws-modules/acm/aws"
+  providers = {
+    aws = aws.us-east-1
+  }
 
-#   providers = {
-#     aws = aws.us-east-1
-#   }
+  domain_name = "support.xayn.com"
+  zone_id     = var.hosted_zone_id
 
-#   domain_name = "support.xayn.com"
-#   zone_id     = var.hosted_zone_id
+  subject_alternative_names = [
+    "*.support.xayn.com",
+  ]
 
-#   subject_alternative_names = [
-#     "*.support.xayn.com",
-#   ]
+  wait_for_validation = true
 
-#   wait_for_validation = true
+  tags = var.tags
+}
 
-#   tags = var.tags
-# }
+module "cdn" {
+  source = "terraform-aws-modules/cloudfront/aws"
 
-# module "cdn" {
-#   source = "terraform-aws-modules/cloudfront/aws"
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_All"
+  retain_on_delete    = false
+  wait_for_deployment = false
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   price_class         = "PriceClass_All"
-#   retain_on_delete    = false
-#   wait_for_deployment = false
+  create_origin_access_identity = true
+  origin_access_identities = {
+    s3_bucket_one = "My awesome CloudFront can access"
+  }
 
-#   create_origin_access_identity = true
-#   origin_access_identities = {
-#     s3_bucket_one = "My awesome CloudFront can access"
-#   }
+  origin = {
+    s3_one = {
+      domain_name = aws_s3_bucket.static_website.bucket_domain_name
+      s3_origin_config = {
+        origin_access_identity = "s3_bucket_one"
+      }
+    }
+  }
 
-#   origin = {
-#     s3_one = {
-#       domain_name = aws_s3_bucket.static_website.bucket_domain_name
-#       s3_origin_config = {
-#         origin_access_identity = "s3_bucket_one"
-#       }
-#     }
-#   }
+  default_cache_behavior = {
+    target_origin_id       = "s3_one"
+    viewer_protocol_policy = "redirect-to-https"
 
-#   default_cache_behavior = {
-#     target_origin_id       = "s3_one"
-#     viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+    query_string    = true
+  }
 
-#     allowed_methods = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods  = ["GET", "HEAD"]
-#     compress        = true
-#     query_string    = true
-#   }
-
-#   viewer_certificate = {
-#     acm_certificate_arn = module.acm.acm_certificate_arn
-#     ssl_support_method  = "sni-only"
-#   }
-# }
+  viewer_certificate = {
+    acm_certificate_arn = module.acm.acm_certificate_arn
+    ssl_support_method  = "sni-only"
+  }
+}
 
 # resource "aws_cloudfront_distribution" "s3_distribution" {
 #   origin {
@@ -127,12 +127,23 @@ resource "aws_s3_bucket_website_configuration" "bucket_website" {
 #   }
 # }
 
-resource "aws_route53_record" "this" {
+resource "aws_route53_record" "record_ipv4" {
   zone_id = var.hosted_zone_id
   name    = var.url_name
   type    = "A"
   alias {
-    name                   = aws_s3_bucket_website_configuration.bucket_website.website_domain
+    name                   = module.cdn.outputs.cloudfront_distribution_domain_name
+    zone_id                = aws_s3_bucket.static_website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "record_ipv6" {
+  zone_id = var.hosted_zone_id
+  name    = var.url_name
+  type    = "AAAA"
+  alias {
+    name                   = module.cdn.outputs.cloudfront_distribution_domain_name
     zone_id                = aws_s3_bucket.static_website.hosted_zone_id
     evaluate_target_health = false
   }

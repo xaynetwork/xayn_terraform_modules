336df7867b7c6defbbfcfa92a5d93c0a7ac5bc40
Robert Steiner <robertt.debug@gmail.com>
2023-01-16 12:28:23 +0100

    ET-3836 redirecting attlasian (#15)
    * Creating redirection module
    
    * Formatting
    
    * Conf s3
    
    * Ch
    
    * Ch
    
    * Ch modules
    
    * Adding cf
    
    * ch
    
    * impl redirect
    
    Co-authored-by: Jorge E. Gamboa G <jorge.gamboa@xayn.com>

---
+++ internal/redirection/README.md
@@ -0,0 +1,20 @@
+# AWS Terraform module for redirecting websites with S3
+
+Terraform module which creates a S3 bucket that will be used for redirection to a certain website.
+
+## Usage
+
+```hcl
+module "redirection" {
+  source = "../../modules/redirection"
+
+  domain_name    = "example.xayn.com"
+  hosted_zone_id = "my_hosted_zone"
+  host_name      = "example.example.com/example"
+
+  # Optional
+  tags = {
+    Environment = "Test"
+  }
+}
+```
---
+++ internal/redirection/variables.tf
@@ -0,0 +1,20 @@
+variable "domain_name" {
+  description = "The name of domain that is used for the redirection"
+  type        = string
+}
+
+variable "hosted_zone_id" {
+  description = "The ID of the hosted zone in Route 53"
+  type        = string
+}
+
+variable "host_name" {
+  description = "Name of the host where requests are redirected"
+  type        = string
+}
+
+variable "tags" {
+  description = "Map of tags for the deployment"
+  type        = map(string)
+  default     = {}
+}

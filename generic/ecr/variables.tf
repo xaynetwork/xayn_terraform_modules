0e9a43fb4514c73d4a78d753c271c73cc10d0bd5
Simon Joecks <simon.joecks@gmail.com>
2023-01-16 13:53:21 +0100

    Add ECR module

---
+++ generic/ecr/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "The name of the ECR repository"
+  type        = string
+}
+
+variable "tags" {
+  description = "Custom tags to set on the underlining resources"
+  type        = map(string)
+  default     = {}
+}
+    

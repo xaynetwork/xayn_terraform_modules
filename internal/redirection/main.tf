d71973fcab64fbbc8f479a5b7ebc08cbb5356ea6
Robert Steiner <robertt.debug@gmail.com>
2023-01-16 12:28:23 +0100

    ET-3836 redirecting attlasian (#15)
    * Creating redirection module
    
    * Formatting
    
    * Conf s3
    
    * Ch
    
    * Ch
    
    * Ch modules
    
    * Adding cf
    
    * ch
    
    * impl redirect
    
    Co-authored-by: Jorge E. Gamboa G <jorge.gamboa@xayn.com>

---
+++ internal/redirection/main.tf
@@ -0,0 +1,114 @@
+locals {
+  origin_id = "redirect-${var.domain_name}"
+}
+
+resource "aws_s3_bucket" "redirect" {
+  bucket = var.domain_name
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect" {
+  bucket = aws_s3_bucket.redirect.bucket
+
+  redirect_all_requests_to {
+    host_name = var.host_name
+  }
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "4.3.1"
+
+  providers = {
+    aws = aws.us-east-1
+  }
+
+  domain_name = var.domain_name
+  zone_id     = var.hosted_zone_id
+
+  subject_alternative_names = [
+    "www.${var.domain_name}"
+  ]
+
+  wait_for_validation = true
+
+  tags = var.tags
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.redirect.website_endpoint
+    origin_id   = local.origin_id
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  default_cache_behavior {
+    target_origin_id       = local.origin_id
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = false
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  aliases = [var.domain_name, "www.${var.domain_name}"]
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = module.acm.acm_certificate_arn
+    minimum_protocol_version = "TLSv1"
+    ssl_support_method       = "sni-only"
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route53_record" "redirect" {
+  zone_id = var.hosted_zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "redirect_www" {
+  zone_id = var.hosted_zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}

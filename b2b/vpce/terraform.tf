11d35edccce637737f43b3c4c2f257ef7691756f
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-01-18 14:36:57 +0100

    Updating provider and terraform

--- b2b/vpce/terraform.tf
+++ b2b/vpce/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.3.3"
+  required_version = "1.3.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.37.0"
+      version = "4.50.0"
     }
     ec = {
       source  = "elastic/ec"

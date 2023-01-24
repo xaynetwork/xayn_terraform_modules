17dd0bebd89a5a18484cb921cddf828c76154a6d
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-01-24 14:26:06 +0100

    Ch output

--- internal/domains/outputs.tf
+++ internal/domains/outputs.tf
@@ -1,9 +1,9 @@
 output "hosted_zone_id" {
   description = "Zone ID of the hosted domain"
-  value = module.zones.output.route53_zone_zone_id
+  value = module.zones.outputs.route53_zone_zone_id
 }
 
 output "name_servers" {
   description = "Name servers of the hosted domain"
-  value = module.zones.output.route53_zone_name_servers
+  value = module.zones.outputs.route53_zone_name_servers
 }

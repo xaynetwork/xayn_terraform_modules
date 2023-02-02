09b37876cc8c06aa26a380c8ccb8dbf285d74a80
Simon Joecks <simon.joecks@gmail.com>
2023-02-02 17:46:13 +0100

    Make Cors request optional

--- embedding/api_gateway/proxy.tf
+++ embedding/api_gateway/proxy.tf
@@ -77,9 +77,10 @@ resource "aws_api_gateway_deployment" "tenant" {
       var.usage_plan_api_key_id,
       var.usage_plan_quota_settings,
       var.usage_plan_throttle_settings,
-      aws_api_gateway_method.options_cors.id,
-      aws_api_gateway_integration.options_cors.id
-    ]))
+      var.enable_cors ? aws_api_gateway_method.options_cors[0].id : 0,
+      var.enable_cors ? aws_api_gateway_integration.options_cors[0].id : 0
+      ]
+    ))
   }
 
   lifecycle {
@@ -148,6 +149,7 @@ resource "aws_api_gateway_usage_plan_key" "api_key" {
 # are performed by the browser therefore it can't contain our api token
 # the request is handled by the backend
 resource "aws_api_gateway_method" "options_cors" {
+  count              = var.enable_cors ? 1 : 0
   rest_api_id        = aws_api_gateway_rest_api.tenant.id
   resource_id        = aws_api_gateway_resource.proxy.id
   http_method        = "OPTIONS"
@@ -156,9 +158,10 @@ resource "aws_api_gateway_method" "options_cors" {
 }
 
 resource "aws_api_gateway_integration" "options_cors" {
+  count                   = var.enable_cors ? 1 : 0
   rest_api_id             = aws_api_gateway_rest_api.tenant.id
   resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.options_cors.http_method
+  http_method             = aws_api_gateway_method.options_cors[0].http_method
   type                    = "HTTP_PROXY"
   integration_http_method = "ANY"
   uri                     = "http://${var.nlb_dns_name}/{proxy}"

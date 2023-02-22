508ab2f52bb250b13b23e04b2024f9b37d03d9b3
Simon Joecks <simon.joecks@gmail.com>
2023-02-22 15:33:04 +0100

    Evalue that rules should not have more than 5 conditions

--- generic/service/service/variables.tf
+++ generic/service/service/variables.tf
@@ -51,6 +51,10 @@ variable "alb" {
       })
   })) })
   default = null
+  validation {
+    condition     = alltrue([for r in try(var.alb.rules, []) : ((r.routing_header_condition == null ? 0 : 1) + length(r.routing_path_pattern)) <= 5])
+    error_message = "Can not exceed 5 conditions per rule. Each path and the hearder condition counts as condition!"
+  }
 }
 
 

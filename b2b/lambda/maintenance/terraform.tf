8556574952196691cec04eb39b76be4b9b9057e5
Simon Joecks <simon.joecks@gmail.com>
2023-03-02 11:16:12 +0100

    Use determanistic zip to create stable archives when uploading to lamnda

--- b2b/lambda/maintenance/terraform.tf
+++ b2b/lambda/maintenance/terraform.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.50.0"
     }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "2.2.0"
-    }
     external = {
       source  = "hashicorp/external"
       version = "2.2.3"

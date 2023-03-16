f99cb3e7145b576db9d10c1002b4bcce7a261346
Simon Joecks <simon.joecks@gmail.com>
2023-03-16 18:02:56 +0100

    WIP:  migration task

---
+++ b2b/service/migration_task/main.tf
@@ -0,0 +1,116 @@
+module "task_role" {
+  source = "../../../generic/service/role"
+
+  description = "${var.tenant}'s task execution role for users API ECS service"
+  path        = "/${var.tenant}/"
+  prefix      = "${title(var.tenant)}Migration"
+  tags        = var.tags
+}
+
+module "secret_policy" {
+  source = "../../../generic/service/secret_policy"
+
+  role_name          = module.task_role.name
+  ssm_parameter_arns = [var.elasticsearch_password_ssm_parameter_arn, var.postgres_password_ssm_parameter_arn]
+  description        = "Allow users api service access to parameter store"
+  path               = "/${var.tenant}/"
+  prefix             = "${title(var.tenant)}Migration"
+  tags               = var.tags
+}
+
+module "security_group" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-security-group?ref=v4.16.0"
+
+  name        = "${var.tenant}-migration-sg"
+  description = "Allow from ALB inbound traffic, Allow all egress traffic (Docker)"
+  vpc_id      = var.vpc_id
+
+  ingress_with_source_security_group_id = [
+    {
+      description              = "Allow from ALB inbound traffic on container port"
+      from_port                = var.container_port
+      to_port                  = var.container_port
+      protocol                 = "tcp"
+      source_security_group_id = var.alb_security_group_id
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      description = "Allow all egress traffic (Docker)"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+  }]
+  tags = var.tags
+}
+
+module "service" {
+  source = "../../../generic/service/service"
+
+  name               = "${var.tenant}-migration"
+  security_group_ids = [module.security_group.security_group_id]
+
+  cluster_id = var.cluster_id
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  alb = null
+
+  container_cpu           = var.container_cpu
+  container_memory        = var.container_memory
+  container_image         = var.container_image
+  container_port          = var.container_port
+  desired_count           = var.desired_count
+  task_execution_role_arn = module.task_role.arn
+  environment = {
+    XAYN_WEB_API__NET__BIND_TO                              = "0.0.0.0:${var.container_port}"
+    XAYN_WEB_API__STORAGE__ELASTIC__URL                     = var.elasticsearch_url
+    XAYN_WEB_API__STORAGE__ELASTIC__INDEX_NAME              = var.elasticsearch_index
+    XAYN_WEB_API__STORAGE__ELASTIC__USER                    = var.elasticsearch_username
+    XAYN_WEB_API__STORAGE__POSTGRES__BASE_URL               = "${var.postgres_url}/${var.tenant}"
+    XAYN_WEB_API__STORAGE__POSTGRES__USER                   = var.postgres_username
+    XAYN_WEB_API__STORAGE__POSTGRES__APPLICATION_NAME       = var.tenant
+    XAYN_WEB_API__PERSONALIZATION__MAX_COIS_FOR_KNN         = var.max_cois
+    XAYN_WEB_API__PERSONALIZATION__DEFAULT_NUMBER_DOCUMENTS = 10
+    XAYN_WEB_API__NET__KEEP_ALIVE                           = var.keep_alive
+    XAYN_WEB_API__NET__CLIENT_REQUEST_TIMEOUT               = var.request_timeout
+    XAYN_WEB_API__LOGGING__LEVEL                            = var.logging_level
+  }
+  secrets = {
+    XAYN_WEB_API__STORAGE__ELASTIC__PASSWORD  = var.elasticsearch_password_ssm_parameter_arn
+    XAYN_WEB_API__STORAGE__POSTGRES__PASSWORD = var.postgres_password_ssm_parameter_arn
+  }
+
+  tags = var.tags
+}
+
+module "asg" {
+  source = "../../../generic/service/asg"
+
+  cluster_name = var.cluster_name
+  service_name = module.service.name
+  min_tasks    = var.desired_count
+  max_tasks    = var.max_count
+}
+
+# CloudWatch alarms
+data "aws_caller_identity" "current" {}
+module "alarms" {
+  providers = {
+    aws.monitoring-account = aws.monitoring-account
+  }
+  source = "../../../generic/alarms/ecs_service"
+
+  account_id = data.aws_caller_identity.current.account_id
+  prefix     = "${data.aws_caller_identity.current.account_id}_"
+
+  cluster_name   = var.cluster_name
+  service_name   = module.service.name
+  log_group_name = module.service.log_group_name
+
+  cpu_usage = var.alarm_cpu_usage
+  log_error = var.alarm_log_error
+
+  tags = var.tags
+}
---
+++ b2b/service/migration_task/variables.tf
@@ -0,0 +1,162 @@
+variable "tenant" {
+  description = "Name of the tenant"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9]{2,22}$", var.tenant))
+    error_message = "Only alphanumeric characters are allowed in 'tenant', and must be 2-22 characters"
+  }
+}
+
+variable "cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+}
+
+variable "cluster_id" {
+  description = "ID of the ECS cluster"
+  type        = string
+}
+
+## network
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "VPC subnet IDs to launch in the ECS service"
+  type        = list(string)
+}
+
+## task
+variable "container_image" {
+  description = "Specifies value of the container image"
+  type        = string
+}
+
+variable "elasticsearch_url" {
+  description = "Name of the Elasticsearch URL"
+  type        = string
+}
+
+variable "elasticsearch_username" {
+  description = "Name of the Elasticsearch username"
+  type        = string
+}
+
+variable "elasticsearch_index" {
+  description = "Name of the Elasticsearch index"
+  type        = string
+}
+
+variable "elasticsearch_password_ssm_parameter_arn" {
+  description = "ARN of the Elasticsearch password SSM parameter"
+  type        = string
+}
+
+variable "postgres_url" {
+  description = "Postgres URL"
+  type        = string
+}
+
+variable "postgres_username" {
+  description = "Postgres username"
+  type        = string
+}
+
+variable "postgres_password_ssm_parameter_arn" {
+  description = "ARN of the postgres password SSM parameter"
+  type        = string
+}
+
+# The keep-alive default is 61. So the service timeout is higher than the ALB timeout
+variable "keep_alive" {
+  description = "Keep alive timeout for services in seconds"
+  type        = string
+  default     = "61"
+}
+
+# A "request_timeout"of "0" means that it is disabled
+variable "request_timeout" {
+  description = "Client request timeout for services in seconds"
+  type        = string
+  default     = "0"
+}
+
+variable "logging_level" {
+  description = "Log level of rust apis"
+  type        = string
+  default     = "INFO"
+}
+
+variable "max_cois" {
+  description = "The max number of calls to elasticsearch."
+  type        = string
+  default     = "5"
+}
+
+## alb
+variable "alb_listener_arn" {
+  description = "ARN of the ALB listener"
+  type        = string
+}
+
+variable "alb_listener_port" {
+  description = "Port of the ALB listener"
+  type        = number
+}
+
+variable "alb_security_group_id" {
+  description = "Security group ID of the ALB"
+  type        = string
+}
+
+# optional parameters
+variable "container_port" {
+  description = "Port of the container"
+  type        = number
+  default     = 8000
+}
+
+variable "container_cpu" {
+  description = "The number of cpu units the ECS container agent reserves for the container"
+  type        = number
+  default     = 256
+}
+
+variable "container_memory" {
+  description = "The hard limit of memory (in MiB) to present to the task"
+  type        = number
+  default     = 512
+}
+
+variable "desired_count" {
+  description = "Number of instances of the task definition to place and keep running"
+  type        = number
+  default     = 2
+}
+
+variable "max_count" {
+  description = "Max number of scalable service tasks"
+  type        = number
+  default     = 4
+}
+
+variable "alarm_cpu_usage" {
+  description = "Alarm for Service average CPU usage. Threshold is in percentage."
+  type        = any
+  default     = {}
+}
+
+variable "alarm_log_error" {
+  description = "Alarm for Service error log count."
+  type        = any
+  default     = {}
+}
+
+variable "tags" {
+  description = "Custom tags to set on the underlining resources"
+  type        = map(string)
+  default     = {}
+}

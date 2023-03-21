661ec4d46db55559e19ca7789fcbe34af0fed46b
Robert Debug <robertt.debug@gmail.com>
2023-03-21 18:11:14 +0100

    ignore min/max_capacity changes

--- generic/service/asg/main.tf
+++ generic/service/asg/main.tf
@@ -4,6 +4,13 @@ resource "aws_appautoscaling_target" "service" {
   resource_id        = "service/${var.cluster_name}/${var.service_name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
+
+  lifecycle {
+    ignore_changes = [
+      min_capacity,
+      max_capacity
+    ]
+  }
 }
 
 resource "aws_appautoscaling_policy" "cpu" {

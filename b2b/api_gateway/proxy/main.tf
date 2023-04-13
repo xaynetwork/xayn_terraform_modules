e915695ce29dbe8d0e6acb832c5f0122f60d16e3
Simon Joecks <simon.joecks@gmail.com>
2023-04-13 14:48:29 +0200

    Change Api Gateway timeout to 29sec for all requests and add candidates

--- b2b/api_gateway/proxy/main.tf
+++ b2b/api_gateway/proxy/main.tf
@@ -77,12 +77,16 @@ resource "aws_api_gateway_method" "documents" {
   api_key_required = true
 }
 
+#########
+## Exceptions mainly for timeouts
+######### 
+
 resource "aws_api_gateway_integration" "documents" {
   rest_api_id             = aws_api_gateway_rest_api.tenant.id
   resource_id             = aws_api_gateway_resource.documents.id
   http_method             = aws_api_gateway_method.documents.http_method
   type                    = "HTTP_PROXY"
-  integration_http_method = "POST"
+  integration_http_method = "ANY"
   uri                     = "http://${var.nlb_dns_name}/documents"
   passthrough_behavior    = "WHEN_NO_MATCH"
   connection_type         = "VPC_LINK"
@@ -92,6 +96,24 @@ resource "aws_api_gateway_integration" "documents" {
   }
   timeout_milliseconds = 29000
 }
+
+resource "aws_api_gateway_integration" "candidates" {
+  rest_api_id             = aws_api_gateway_rest_api.tenant.id
+  resource_id             = aws_api_gateway_resource.documents.id
+  http_method             = aws_api_gateway_method.documents.http_method
+  type                    = "HTTP_PROXY"
+  integration_http_method = "PUT"
+  uri                     = "http://${var.nlb_dns_name}/candidates"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  connection_type         = "VPC_LINK"
+  connection_id           = var.nlb_vpc_link_id
+  request_parameters = {
+    "integration.request.header.X-Tenant-Id" = "'${var.tenant}'"
+  }
+  timeout_milliseconds = 29000
+}
+
+#########
 
 resource "aws_api_gateway_deployment" "tenant" {
   depends_on  = [aws_api_gateway_method.proxy]
@@ -107,6 +129,8 @@ resource "aws_api_gateway_deployment" "tenant" {
       aws_api_gateway_method.documents.id,
       aws_api_gateway_integration.documents.id,
       aws_api_gateway_integration.documents.request_parameters,
+      aws_api_gateway_integration.candidates.id,
+      aws_api_gateway_integration.candidates.request_parameters,
       var.enable_usage_plan,
       var.usage_plan_api_key_id,
       var.usage_plan_quota_settings,

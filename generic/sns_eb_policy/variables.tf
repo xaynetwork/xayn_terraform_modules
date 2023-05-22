ff5604d921f63a5916adaeca2d97daedc038d283
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-05-22 17:26:17 +0200

    removing tags

--- generic/sns_eb_policy/variables.tf
+++ generic/sns_eb_policy/variables.tf
@@ -2,9 +2,3 @@ variable "sns_arn" {
   description = "ARN from the SNS topic"
   type        = string
 }
-
-variable "tags" {
-  description = "Map of tags for the deployment"
-  type        = map(string)
-  default     = {}
-}

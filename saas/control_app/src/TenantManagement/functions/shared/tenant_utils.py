41efa927005831757e55d4a5c48e5fecb2099649
Simon Joecks <simon.joecks@gmail.com>
2023-05-25 09:02:27 +0200

    Clean up linting

--- saas/control_app/src/TenantManagement/functions/shared/tenant_utils.py
+++ saas/control_app/src/TenantManagement/functions/shared/tenant_utils.py
@@ -1,14 +1,14 @@
 import secrets
 import string
 
-alphabet = string.ascii_lowercase + string.digits
-pass_alphabet = alphabet + string.ascii_uppercase
+_ALPHABET = string.ascii_lowercase + string.digits
+_PASS_ALPHABET = _ALPHABET + string.ascii_uppercase
 
 # good to read
 # 15 collisions within 10M draws - https://stackoverflow.com/a/56398787/495800
 def create_id():
-    return ''.join(secrets.choice(alphabet) for _ in range(8))
+    return ''.join(secrets.choice(_ALPHABET) for _ in range(8))
 
 
 def create_random_password():
-    return ''.join(secrets.choice(pass_alphabet) for _ in range(16))
+    return ''.join(secrets.choice(_PASS_ALPHABET) for _ in range(16))

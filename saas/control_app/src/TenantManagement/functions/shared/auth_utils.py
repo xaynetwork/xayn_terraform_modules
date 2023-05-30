b0ff12815431a556399a962b1a481ab9ae6d797f
Simon Joecks <simon.joecks@gmail.com>
2023-05-30 10:53:56 +0200

    Fix linting

--- saas/control_app/src/TenantManagement/functions/shared/auth_utils.py
+++ saas/control_app/src/TenantManagement/functions/shared/auth_utils.py
@@ -11,8 +11,8 @@ def try_decode_auth_key(encoded_auth_key: str) -> tuple[str | None, str | None]:
     try:
         decoded = base64.b64decode(encoded_auth_key).decode("utf-8")
         if ":" in decoded:
-            (id, key) = decoded.split(":", maxsplit=1)
-            return (id, key)
+            (_id, key) = decoded.split(":", maxsplit=1)
+            return (_id, key)
 
         return (None, None)
     except binascii.Error:

cdc7e9eaef9ebab0e6a1a34626a6ea75524d388a
Simon Joecks <simon.joecks@gmail.com>
2023-06-06 19:29:34 +0200

    Do not use a temp directory for the makefile
    - easiert to debug
    - less dead files in temp dirs

--- saas/control_app/src/TenantPipeline/build.sh
+++ saas/control_app/src/TenantPipeline/build.sh
@@ -2,7 +2,9 @@
 
 WORKDIR=$1
 pushd $WORKDIR > /dev/null 2>&1
-ARTIFACTS_DIR=$(mktemp -d)
+ARTIFACTS_DIR="$(pwd)/build/pipeline_lambda"
+mkdir -p $ARTIFACTS_DIR
+rm -rf ${ARTIFACTS_DIR}/* > /dev/null 2>&1
 ARTIFACTS_DIR=$ARTIFACTS_DIR make > /dev/null 2>&1
 pushd $ARTIFACTS_DIR > /dev/null 2>&1 && deterministic-zip -r function.zip . > /dev/null 2>&1 && popd > /dev/null 2>&1
 echo { \"output\": \"$ARTIFACTS_DIR/function.zip\" }

da828d6aa08d775024d476eb73b7f9072ef1b73e
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-06-06 15:23:05 +0200

    creating www

--- generic/app_runner_domain/main.tf
+++ generic/app_runner_domain/main.tf
@@ -22,3 +22,11 @@ resource "aws_route53_record" "custom_domain" {
   ttl     = 300
   zone_id = data.aws_route53_zone.this.id
 }
+
+resource "aws_route53_record" "www_custom_domain" {
+  name    = "www.${var.subdomain_name}"
+  type    = "CNAME"
+  records = [local.url_no_protocol]
+  ttl     = 300
+  zone_id = data.aws_route53_zone.this.id
+}

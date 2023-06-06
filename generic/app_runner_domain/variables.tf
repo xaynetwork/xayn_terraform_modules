2ed707c656bc262fb08dae700d26fad4ddb68b4e
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-06-06 10:48:40 +0200

    changing variable

--- generic/app_runner_domain/variables.tf
+++ generic/app_runner_domain/variables.tf
@@ -8,7 +8,7 @@ variable "subdomain_name" {
   description = "Subdomain name for the app runner record."
 }
 
-variable "custom_domain_association_certificate_validation_records" {
+variable "certificate_validation_records" {
   type        = any
   description = "A set of certificate CNAME records used for this domain name."
 }

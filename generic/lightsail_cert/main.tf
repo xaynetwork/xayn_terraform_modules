29b0baa9e3343da16b07dffd89be52dd8b3a26e1
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-06-08 13:17:05 +0200

    Using for eachusing for each

--- generic/lightsail_cert/main.tf
+++ generic/lightsail_cert/main.tf
@@ -3,10 +3,11 @@ data "aws_route53_zone" "this" {
 }
 
 resource "aws_route53_record" "validation_records_linglinger" {
-  count   = length(aws_lightsail_certificate.this.domain_validation_options)
-  name    = aws_lightsail_certificate.this.domain_validation_options[count.index].resource_record_name
-  type    = aws_lightsail_certificate.this.domain_validation_options[count.index].resource_record_type
-  records = [aws_lightsail_certificate.this.domain_validation_options[count.index].resource_record_value]
+  for_each = aws_lightsail_certificate.this.domain_validation_options
+
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  records = [each.value.resource_record_value]
   ttl     = 300
   zone_id = data.aws_route53_zone.this.id
   depends_on = [

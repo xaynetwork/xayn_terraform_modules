87045457ded30e6403297d705e68ddfde3b4c2d0
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-06-08 16:45:34 +0200

    lightsail db

---
+++ generic/lightsail_db/variables.tf
@@ -0,0 +1,31 @@
+variable "db_name" {
+  description = "Name for an automatically created database on creation"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for the master DB user"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Username for the master DB user"
+  type        = string
+}
+
+variable "db_blueprint" {
+  description = "A blueprint describes the major engine version of a database."
+  type        = string
+  default = "postgres_15"
+}
+
+variable "db_bundle_id" {
+  description = "Name for an automatically created database on creation"
+  type        = string
+  default = "micro_1_0"
+}
+
+variable "subdomain_name" {
+  description = " The name of the domain for the container."
+  type        = string
+}

59ba48eeaacbe6999e3fb250877953bd6ac418b8
Simon Joecks <simon.joecks@gmail.com>
2023-06-08 10:03:14 +0200

    Change user id to hex value, in order to avoid the hyphens

---
+++ saas/control_app/src/TenantManagement/tests/unit/tenent_utils_test.py
@@ -0,0 +1,16 @@
+import uuid
+from TenantManagement.functions.shared.tenant_utils import create_id, create_secure_string
+
+def test_two_tenant_ids_should_never_be_the_same():
+    assert create_id() != create_id()
+
+def test_that_tenant_id_can_be_converted_to_uuid4_again():
+    id1hex = create_id()
+    id2 = uuid.UUID(hex=id1hex, version=4)
+
+    assert id1hex == id2.hex
+
+def test_secure_is_long_enough():
+    secure = create_secure_string()
+
+    assert len(secure) >= 20

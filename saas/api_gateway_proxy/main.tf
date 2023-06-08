603b77698e96234928d9f99558a6e2ba53b86ecb
Simon Joecks <simon.joecks@gmail.com>
2023-06-08 15:03:42 +0200

    Enable verification on all changes

--- saas/api_gateway_proxy/main.tf
+++ saas/api_gateway_proxy/main.tf
@@ -68,12 +68,12 @@ resource "aws_api_gateway_resource" "documents" {
 }
 
 resource "aws_api_gateway_method" "documents" {
-  rest_api_id        = aws_api_gateway_rest_api.api.id
-  resource_id        = aws_api_gateway_resource.documents.id
-  http_method        = "ANY"
-  authorization      = "CUSTOM"
-  authorizer_id      = aws_api_gateway_authorizer.lambda_authorizer.id
-  api_key_required   = true
+  rest_api_id      = aws_api_gateway_rest_api.api.id
+  resource_id      = aws_api_gateway_resource.documents.id
+  http_method      = "ANY"
+  authorization    = "CUSTOM"
+  authorizer_id    = aws_api_gateway_authorizer.lambda_authorizer.id
+  api_key_required = true
 }
 
 resource "aws_api_gateway_integration" "documents" {

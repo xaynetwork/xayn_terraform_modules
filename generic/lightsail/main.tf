50f2e1c47782d82576c06e26beb81ead5088013d
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-06-15 15:14:47 +0200

    adding count

--- generic/lightsail/main.tf
+++ generic/lightsail/main.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "this" {
 resource "aws_ecr_repository_policy" "this" {
   count      = var.private_registry_access ? 1 : 0
   repository = var.repository_name
-  policy     = data.aws_iam_policy_document.this.json
+  policy     = data.aws_iam_policy_document.this[0].json
 }
 
 # Lightsail Configuration

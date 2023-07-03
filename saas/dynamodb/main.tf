600c72a0eab17475e8b85ed7b2026dcbbd9dae63
Jorge E. Gamboa G <jorge.gamboa@xayn.com>
2023-07-03 11:46:41 +0200

    Using PIT

--- saas/dynamodb/main.tf
+++ saas/dynamodb/main.tf
@@ -1,16 +1,3 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["backup.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 locals {
   db_name = "saas_tenants"
 }
@@ -25,10 +12,11 @@ module "dynamodb_table" {
   #   0001   | { auth-keys: [...],  name: "John Doe"}
   #   0002   | {...}    
 
-  hash_key         = "id"
-  billing_mode     = "PAY_PER_REQUEST"
-  stream_enabled   = true
-  stream_view_type = "KEYS_ONLY"
+  hash_key                       = "id"
+  billing_mode                   = "PAY_PER_REQUEST"
+  stream_enabled                 = true
+  stream_view_type               = "KEYS_ONLY"
+  point_in_time_recovery_enabled = var.enable_pit
 
   attributes = [
     {
@@ -38,38 +26,4 @@ module "dynamodb_table" {
   ]
 
   tags = var.tags
-}
-
-resource "aws_iam_role" "this" {
-  name               = "aws_backup_role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-  role       = aws_iam_role.this.name
-}
-
-resource "aws_backup_plan" "this" {
-  name = "Dynamodb_backup"
-
-  rule {
-    rule_name         = "daily_dynamodb_backup"
-    target_vault_name = "Default"
-    schedule          = var.backup_frecuency
-
-    lifecycle {
-      delete_after = var.retention_days
-    }
-  }
-}
-
-resource "aws_backup_selection" "this" {
-  iam_role_arn = aws_iam_role.this.arn
-  name         = "dynamodb_backup"
-  plan_id      = aws_backup_plan.this.id
-
-  resources = [
-    module.dynamodb_table.dynamodb_table_arn
-  ]
 }
--- saas/dynamodb/variables.tf
+++ saas/dynamodb/variables.tf
@@ -1,13 +1,7 @@
-variable "retention_days" {
-  description = "The number of days to keep the backups"
-  type        = number
-  default     = 7
-}
-
-variable "backup_frecuency" {
-  description = "How often do we want to do the backup of the dynamodb table(in cron setup)"
-  type        = string
-  default     = "cron(0 12 * * ? *)"
+variable "enable_pit" {
+  description = "Whether to enable point-in-time recovery"
+  type        = bool
+  default     = false
 }
 
 variable "tags" {

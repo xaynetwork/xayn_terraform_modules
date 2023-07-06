7a67ca5ef46155e6ed60b215b94545cb18dce62c
Robert Debug <robertt.debug@gmail.com>
2023-07-06 15:48:09 +0200

    community module

--- saas/api_gateway_proxy/main.tf
+++ saas/api_gateway_proxy/main.tf
@@ -236,7 +236,7 @@ resource "aws_api_gateway_deployment" "api" {
       aws_api_gateway_integration.options_cors_documents.id,
       aws_api_gateway_method.options_cors_documents_proxy.id,
       aws_api_gateway_integration.options_cors_documents_proxy.id,
-      aws_api_gateway_rest_api_policy._silo_management.id
+      data.aws_iam_policy_document._silo_management.json
     ]))
   }
 
--- saas/control_app/outputs.tf
+++ saas/control_app/outputs.tf
@@ -1,19 +1,19 @@
 output "invoke_authentication_arn" {
   description = "ARN to be used for invoking the `authentication` lambda"
-  value       = module.authentication_function.invoke_arn
+  value       = module.authentication_function.lambda_function_invoke_arn
 }
 
 output "authentication_arn" {
   description = "ARN of the the `authentication` lambda"
-  value       = module.authentication_function.arn
+  value       = module.authentication_function.lambda_function_arn
 }
 
 output "provisioning_arn" {
   description = "ARN of the the `provisioning` lambda"
-  value       = module.provisioning_function.arn
+  value       = module.provisioning_function.lambda_function_arn
 }
 
 output "provisioning_role_arn" {
   description = "ARN of the role of the `provisioning` lambda"
-  value       = module.role_prov.arn
+  value       = module.provisioning_function.lambda_role_arn
 }

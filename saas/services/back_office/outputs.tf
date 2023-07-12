8db7530d4e321faaea3431f067bcfdf76a5c6d86
Robert Debug <robertt.debug@gmail.com>
2023-07-12 10:39:54 +0200

    export service values

--- saas/services/back_office/outputs.tf
+++ saas/services/back_office/outputs.tf
@@ -1,3 +1,8 @@
+output "service" {
+  description = "Back office service."
+  value       = module.service
+}
+
 output "alarm_arns" {
   description = "ARNs of the CloudWatch alarms."
   value       = try(module.alarms[0].arns, null)
--- saas/services/front_office/outputs.tf
+++ saas/services/front_office/outputs.tf
@@ -1,3 +1,8 @@
+output "service" {
+  description = "Front office service."
+  value       = module.service
+}
+
 output "alarm_arns" {
   description = "ARNs of the CloudWatch alarms."
   value       = try(module.alarms[0].arns, null)

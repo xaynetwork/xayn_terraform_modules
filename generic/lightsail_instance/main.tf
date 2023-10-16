861a5f1761f563ba4ca5be39d042b566821839c9
Simon Joecks <simon.joecks@gmail.com>
2023-10-16 11:06:18 +0200

    fix user data and dns record

--- generic/lightsail_instance/main.tf
+++ generic/lightsail_instance/main.tf
@@ -1,20 +1,5 @@
 locals {
-  user_script = <<EOT
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker admin
-EOT
+  user_script = file("user_data.sh")
 }
 
 resource "aws_lightsail_instance" "this" {
@@ -32,7 +17,7 @@ data "aws_route53_zone" "this" {
 
 resource "aws_route53_record" "custom_domain" {
   name    = var.subdomain_name
-  type    = "CNAME"
+  type    = "A"
   records = [aws_lightsail_instance.this.public_ip_address]
   ttl     = 300
   zone_id = data.aws_route53_zone.this.id
--- generic/lightsail_instance/outputs.tf
+++ generic/lightsail_instance/outputs.tf
@@ -4,7 +4,7 @@ output "public_ip_address" {
 
 output "user_name" {
   description = "Connect with ssh://username@public_ip after downloading the lightsail key from the console"
-  value = aws_lightsail_instance.this.username
+  value       = aws_lightsail_instance.this.username
 }
 
 output "domain_name" {

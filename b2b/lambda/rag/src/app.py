e532da2a5c604702e900d83390c07a9a63d47545
Robert Debug <robertt.debug@gmail.com>
2023-11-13 17:12:55 +0100

    cleanup

--- b2b/lambda/rag/src/app.py
+++ b2b/lambda/rag/src/app.py
@@ -1,6 +1,5 @@
 #!/bin/env python
 
-import json
 import os
 from typing import Optional
 
@@ -8,19 +7,13 @@ from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.logging import correlation_paths
-from aws_lambda_powertools.utilities.parser import parse, ValidationError
-import boto3
 import requests
 
-from xayn_infra_lib.models import QuestionRequest
-
 aws_region = os.getenv("AWS_REGION")
-sagemaker_endpoint_name = os.getenv("SAGEMAKER_ENDPOINT_NAME")
 nlb_url = os.getenv("NLB_URL")
 
 app = APIGatewayRestResolver()
 logger = Logger()
-session = boto3.Session(region_name=aws_region)
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
@@ -42,10 +35,4 @@ def get_answer():
         )
         print(r.text)
 
-    try:
-        request: QuestionRequest = parse(
-            event=app.current_event.json_body, model=QuestionRequest
-        )
-        return {"tenant_id": tenant_id, "answer": request.question}
-    except ValidationError:
-        return {"status_code": 400, "message": "Invalid question request"}
+    return {"tenant_id": tenant_id}

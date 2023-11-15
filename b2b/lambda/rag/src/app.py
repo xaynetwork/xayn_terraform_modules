b804264c29a4deddbb8ab8aa7becbcfa8dd6ef69
Marin <3746994+petricevich@users.noreply.github.com>
2023-11-15 14:05:30 +0100

    Et 4955 RAG (#154)
    * lambda impl
    
    * add readme
    
    * add RAG
    
    * make naming consistent
    
    * add working SageMaker LLM provider
    
    * add rag lambda, remove rag building blocks
    
    * change requirements.txt
    
    * merge
    
    * remove unnecessary tenant getenv
    
    * format terraform configuration files
    
    * change dependency to xayn_rag master
    
    * Update b2b/lambda/rag/src/requirements.txt
    
    Co-authored-by: Robert Steiner <robertt.debug@gmail.com>
    
    * Update b2b/lambda/rag/src/app.py
    
    Co-authored-by: Robert Steiner <robertt.debug@gmail.com>
    
    * Update b2b/lambda/rag/src/requirements.txt
    
    Co-authored-by: Robert Steiner <robertt.debug@gmail.com>
    
    * Update b2b/lambda/rag/src/app.py
    
    Co-authored-by: Robert Steiner <robertt.debug@gmail.com>
    
    ---------
    
    Co-authored-by: Robert Debug <robertt.debug@gmail.com>

--- b2b/lambda/rag/src/app.py
+++ b2b/lambda/rag/src/app.py
@@ -1,19 +1,30 @@
 #!/bin/env python
 
+import json
 import os
 from typing import Optional
 
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools import Logger
-from aws_lambda_powertools.logging import correlation_paths
-import requests
+from aws_lambda_powertools.logging import correlation_paths, utils
+from aws_lambda_powertools.utilities.parser import parse, ValidationError
 
-aws_region = os.getenv("AWS_REGION")
+from xayn_infra_lib.models import QuestionRequest
+from xayn_rag.retrieval import Xayn
+from xayn_rag.llm_templating import em_german_rag
+from xayn_rag.generation import huggingfaceTGI
+
 nlb_url = os.getenv("NLB_URL")
+frontoffice_base_url = os.getenv("FRONTOFFICE_BASE_URL")
+frontoffice_token = os.getenv("FRONTOFFICE_TOKEN")
+llm_url = os.getenv("LLM_URL")
+llm_bearer_token = os.getenv("LLM_BEARER_TOKEN")
+use_top_n_results = os.getenv("USE_TOP_N_RESULTS", "2")
 
 app = APIGatewayRestResolver()
 logger = Logger()
+utils.copy_config_to_registered_loggers(source_logger=logger)
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
@@ -25,14 +36,37 @@ def lambda_handler(event: dict, context: LambdaContext) -> dict:
 def get_answer():
     tenant_id: Optional[str] = app.current_event.request_context.authorizer.principal_id
 
-    if tenant_id is not None:
-        r = requests.post(
-            f"{nlb_url}/semantic_search",
-            headers={"X-Tenant-Id": tenant_id},
-            json={
-                "document": {"query": "test"},
-            },
+    try:
+        request: QuestionRequest = parse(
+            event=app.current_event.json_body, model=QuestionRequest
         )
-        print(r.text)
+    except ValidationError:
+        return {"status_code": 400, "message": "Invalid question request"}
 
-    return {"tenant_id": tenant_id}
+    search_engine = Xayn(
+        endpoint=frontoffice_base_url or nlb_url,
+        frontoffice_token=frontoffice_token,
+        tenant_id=tenant_id,
+    )
+    search_results = search_engine.search(request.query, int(use_top_n_results))
+    if search_results is None:
+        return {"status_code": 500, "message": "Search failed"}
+    
+    llm = huggingfaceTGI(llm_url, llm_bearer_token)
+
+    turns = []
+    for search_result in search_results:
+        turns.append(
+            {
+                "speaker": "context",
+                "text": search_result,
+            }
+        )
+    turns.append({"speaker": "question", "text": request.query})
+
+    try:
+        answer = llm.generate(em_german_rag(turns))
+    except Exception as e:
+        return {"status_code": 409, "message": str(e)}
+
+    return {"status_code": 200, "response": answer}
--- b2b/lambda/rag/tf-resources/local.tfvars
+++ b2b/lambda/rag/tf-resources/local.tfvars
@@ -1,8 +1,11 @@
 # define required varables here:
 environment_variables = {
-  "AWS_REGION"              = "eu-central-1"
-  "SAGEMAKER_ENDPOINT_NAME" = "gte-base-endpoint"
-  # for local development NLB_URL can point to a local front office
-  "NLB_URL" = "http://nlb-dev-649f929e99958326.elb.eu-central-1.amazonaws.com"
+  "AWS_REGION"           = "eu-central-1"
+  "NLB_URL"              = ""
+  "FRONTOFFICE_BASE_URL" = ""
+  "FRONTOFFICE_TOKEN"    = ""
+  "LLM_URL"              = ""
+  "LLM_BEARER_TOKEN"     = ""
 }
+
 layers = ["arn:aws:lambda:eu-central-1:017000801446:layer:AWSLambdaPowertoolsPythonV2-Arm64:46"]

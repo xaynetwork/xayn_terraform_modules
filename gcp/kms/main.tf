03aec6c2f5e6aaf14355b141b85a9d13ee144974
Jorge E. Gamboa G. <jogamboa4@gmail.com>
2024-01-17 16:42:54 +0100

    Changing name and time

--- gcp/kms/main.tf
+++ gcp/kms/main.tf
@@ -1,5 +1,5 @@
 resource "google_kms_key_ring" "this" {
-  name     = "${var.name}-ring"
+  name     = "${var.name}-keyring"
   location = var.region
   project  = var.project
 }
@@ -7,8 +7,8 @@ resource "google_kms_key_ring" "this" {
 resource "google_kms_crypto_key" "this" {
   name                       = var.name
   key_ring                   = var.create_key_ring ? google_kms_key_ring.this.id : var.key_ring
-  rotation_period            = "100000s"
-  destroy_scheduled_duration = 240
+  rotation_period            = "604800s"
+  destroy_scheduled_duration = "604800s"
 
   lifecycle {
     prevent_destroy = true

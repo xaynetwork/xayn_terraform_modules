ab7ab5a0e86e2b965e966ab4048ac586d3cbeb93
Jorge E. Gamboa G. <jogamboa4@gmail.com>
2024-01-23 10:32:01 +0100

    Removing default service_list

--- gcp/project/variables.tf
+++ gcp/project/variables.tf
@@ -21,8 +21,4 @@ variable "billing_account_id" {
 variable "gcp_service_list" {
   description = "The list of apis necessary for the project"
   type        = list(string)
-  default = [
-    "cloudresourcemanager.googleapis.com",
-    "serviceusage.googleapis.com"
-  ]
 }

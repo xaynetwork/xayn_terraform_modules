9d98aa2e9c608fb23017095872146285cc233b58
Jorge E. Gamboa G. <jogamboa4@gmail.com>
2024-01-26 14:41:41 +0100

    Removing from project

--- gcp/project/variables.tf
+++ gcp/project/variables.tf
@@ -22,9 +22,3 @@ variable "gcp_service_list" {
   description = "The list of apis necessary for the project"
   type        = list(string)
 }
-
-variable "gcp_service_identity" {
-  description = "The list of apis necessary for the project"
-  type        = list(string)
-  default     = []
-}

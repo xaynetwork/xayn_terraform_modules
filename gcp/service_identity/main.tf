659bc076bf6a823c09ae3148b3d5a50a90e00180
Jorge E. Gamboa G. <jogamboa4@gmail.com>
2024-01-26 16:29:28 +0100

    Fixing details

--- gcp/service_identity/main.tf
+++ gcp/service_identity/main.tf
@@ -9,8 +9,8 @@ resource "google_project_service_identity" "gcp_service_identity" {
 # on the key level.
 # This can be granted via the `cloud kms Admin` role on the key level.
 resource "google_kms_crypto_key_iam_member" "crypto_key" {
-  for_each      = toset(var.grant_access_to_kms_crypro_keys)
-  crypto_key_id = each.value
+  count         = length(var.gcp_service_identity)
+  crypto_key_id = var.grant_access_to_kms_crypro_key
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_project_service_identity.gcp_service_identity[count.index].email}"
 }
--- gcp/service_identity/variables.tf
+++ gcp/service_identity/variables.tf
@@ -9,8 +9,7 @@ variable "gcp_service_identity" {
   default     = []
 }
 
-variable "grant_access_to_kms_crypro_keys" {
-  description = "A list of keys that the Service account should receive Encryter / Descrpter role"
-  type        = list(string)
-  default     = []
+variable "grant_access_to_kms_crypro_key" {
+  description = "The key that the Service account should receive Encryter / Descrpter role"
+  type        = string
 }

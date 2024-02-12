df3d2a156e3d5dec2db705178cb221e35c0f6d60
Simon Joecks <simon.joecks@gmail.com>
2024-02-12 17:03:17 +0100

    Fix issue where a map of any is converted in a map of strings

--- generic/lambda/invoke/variables.tf
+++ generic/lambda/invoke/variables.tf
@@ -1,13 +1,15 @@
 variable "create_payload" {
   description = "The payload that is sent to the lambda function during creation"
-  type        = map(string)
-
+  # can not use map(any) because all values witll be converted to the same type, in this case string
+  type = any
 }
 
 variable "delete_payload" {
   description = "The payload that is sent to the lambda function during deletion"
-  type        = map(string)
+  # can not use map(any) because all values witll be converted to the same type, in this case string
+  type = any
 }
+
 
 variable "function_arn" {
   description = "The ARN of the lambda function that is invoked with the create and delete json payload"

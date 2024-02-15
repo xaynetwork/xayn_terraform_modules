f8b6232718c237afcce69024a7c5a86d6866b219
Jorge E. Gamboa G. <jogamboa4@gmail.com>
2024-02-15 13:59:19 -0600

    Creating S3 bucket with access to OP

---
+++ otc/s3_bucket_op/main.tf
@@ -0,0 +1,47 @@
+locals {
+  bucket_name = replace(lower(var.bucket_name), "_", "-")
+}
+
+provider "onepassword" {
+  account = "https://xaynag.1password.com/"
+}
+
+data "onepassword_item" "ak" {
+  vault = var.vault_id
+  uuid  = var.access_key_uid
+}
+
+data "onepassword_item" "sk" {
+  vault = var.vault_id
+  uuid  = var.secret_key_uid
+}
+
+provider "opentelekomcloud" {
+  auth_url    = "https://iam.${var.region}.otc.t-systems.com/v3"
+  tenant_name = var.region
+  access_key  = data.onepassword_item.ak.password
+  secret_key  = data.onepassword_item.sk.password
+}
+
+resource "opentelekomcloud_obs_bucket" "tf_remote_state" {
+  bucket     = local.bucket_name
+  acl        = "private"
+  versioning = true
+  server_side_encryption {
+    algorithm  = "kms"
+    kms_key_id = opentelekomcloud_kms_key_v1.tf_remote_state_bucket_kms_key.id
+  }
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+resource "opentelekomcloud_kms_key_v1" "tf_remote_state_bucket_kms_key" {
+  key_alias       = "${local.bucket_name}-key-${random_id.id.hex}"
+  key_description = "${local.bucket_name} encryption key"
+  pending_days    = 7
+  is_enabled      = "true"
+}
+
+
---
+++ otc/s3_bucket_op/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "v1.3.7"
+  required_providers {
+    opentelekomcloud = {
+      source  = "opentelekomcloud/opentelekomcloud"
+      version = "1.29.0"
+    }
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "1.4.1"
+    }
+  }
+}

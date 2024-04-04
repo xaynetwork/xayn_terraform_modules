2a5dc05a98fb2f1422ab122c78aae437d6a5fefe
Jorge E. Gamboa G. <jogamboa4@gmail.com>
2024-04-04 11:27:54 +0200

    Updating provider

--- otc/dcs/terraform.tf
+++ otc/dcs/terraform.tf
@@ -5,5 +5,9 @@ terraform {
       source  = "opentelekomcloud/opentelekomcloud"
       version = ">=1.29.0"
     }
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "1.4.1"
+    }
   }
 }

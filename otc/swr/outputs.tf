695ff19220ca1be0e09e2ed99b9ed1efa7f3fa68
Alex Litvinenko <alexander.litvinenko.external@xayn.com>
2024-04-22 12:15:02 +0200

    Make linter happy

--- otc/swr/outputs.tf
+++ otc/swr/outputs.tf
@@ -1,7 +1,7 @@
 output "ci_cd_docker_credentials" {
   sensitive = true
   value = {
-    login    = format("%s@%s", "${var.name}-ci-cd", opentelekomcloud_identity_credential_v3.ci_cd.access)
+    login             = format("%s@%s", "${var.name}-ci-cd", opentelekomcloud_identity_credential_v3.ci_cd.access)
     login_key_command = <<SHELL
 printf "${opentelekomcloud_identity_credential_v3.ci_cd.access}" | openssl dgst -binary -sha256 -hmac "${opentelekomcloud_identity_credential_v3.ci_cd.secret}" | od -An -vtx1 | sed 's/[ \n]//g' | sed 'N;s/\n//'
 SHELL

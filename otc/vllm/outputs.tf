39080becdb5d4169d23e6a2871e6034bcc9e5ad7
Alex Litvinenko <alexander.litvinenko.external@xayn.com>
2024-04-24 23:18:09 +0200

    ET-5960 Add storage for vLLM

---
+++ otc/vllm/outputs.tf
@@ -0,0 +1,13 @@
+output "bucket_domain_name" {
+  value = opentelekomcloud_obs_bucket.models.bucket_domain_name
+}
+
+output "bucket_access_key" {
+  value = opentelekomcloud_identity_credential_v3.user_aksk.access
+  sensitive = true
+}
+
+output "bucket_secret_key" {
+  value = opentelekomcloud_identity_credential_v3.user_aksk.secret
+  sensitive = true
+}
---
+++ otc/vllm/terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = "v1.3.7"
+  required_providers {
+    opentelekomcloud = {
+      source  = "opentelekomcloud/opentelekomcloud"
+      version = "1.36.5"
+    }
+  }
+}

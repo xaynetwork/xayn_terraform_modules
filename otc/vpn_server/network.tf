33a1068fbff7fbfe364894ed08bddbdb3398adcf
Alex Litvinenko <alexander.litvinenko.external@xayn.com>
2024-05-22 17:04:46 +0200

    ET-6017 WireGuard VPN (#197)
    * ET-6017 Add Terraform module for WireGuard VPN server
    
    * Remove unused variables
    
    * Rename security group for better clarity
    
    * Make TF linter happy
    
    * Remove debug code
    
    * Don't use SSH key pair
    
    * Open SSH port only when required

---
+++ otc/vpn_server/network.tf
@@ -0,0 +1,43 @@
+data "opentelekomcloud_vpc_subnet_v1" "this" {
+  id = var.subnet_id
+}
+
+resource "opentelekomcloud_networking_secgroup_v2" "wireguard_server" {
+  name = "wireguard-server"
+}
+
+resource "opentelekomcloud_networking_secgroup_rule_v2" "wireguard" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = var.vpn_port
+  port_range_max    = var.vpn_port
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = opentelekomcloud_networking_secgroup_v2.wireguard_server.id
+}
+
+resource "opentelekomcloud_networking_secgroup_rule_v2" "ssh" {
+  count = length(var.ssh_public_keys) > 0 ? 1 : 0
+
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = opentelekomcloud_networking_secgroup_v2.wireguard_server.id
+}
+
+resource "opentelekomcloud_networking_secgroup_rule_v2" "peer" {
+  for_each = {
+    for peer in local.peers : peer.name => peer
+  }
+
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = each.value.server_port
+  port_range_max    = each.value.server_port
+  remote_ip_prefix  = data.opentelekomcloud_vpc_subnet_v1.this.cidr
+  security_group_id = opentelekomcloud_networking_secgroup_v2.wireguard_server.id
+}
---
+++ otc/vpn_server/wireguard.tf
@@ -0,0 +1,41 @@
+resource "wireguard_asymmetric_key" "server" {}
+
+resource "wireguard_asymmetric_key" "peer" {
+  for_each = {
+    for peer in local.peers : peer.name => peer
+  }
+}
+
+data "wireguard_config_document" "server" {
+  private_key = wireguard_asymmetric_key.server.private_key
+  listen_port = var.vpn_port
+  addresses   = [var.vpn_network_mask]
+
+  dynamic "peer" {
+    for_each = {
+      for peer in local.peers : peer.name => peer
+    }
+    content {
+      allowed_ips          = [peer.value.ip]
+      persistent_keepalive = 25
+      public_key           = wireguard_asymmetric_key.peer[peer.key].public_key
+    }
+  }
+}
+
+data "wireguard_config_document" "peer" {
+  for_each = {
+    for peer in local.peers : peer.name => peer
+  }
+
+  private_key = wireguard_asymmetric_key.peer[each.key].private_key
+  addresses   = [each.value.ip]
+
+  peer {
+    # Only communication with VPN server is allowed
+    allowed_ips          = [local.server_ip]
+    endpoint             = format("%s:%s", opentelekomcloud_vpc_eip_v1.this.publicip[0].ip_address, var.vpn_port)
+    persistent_keepalive = 25
+    public_key           = wireguard_asymmetric_key.server.public_key
+  }
+}
